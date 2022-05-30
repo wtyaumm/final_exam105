2a0bc30d7ab30f0f213607ea2fc2f56c8e968260
wtyaumm <muaymiimuay@gmail.com>
2022-05-30 18:46:59 +0700

    task 4

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,13 +7,18 @@ import {
   Button,
   Input,
   Label,
+  Modal,
+  ModalBody,
+  CardImgOverlay,
 } from "reactstrap";
 import style from "../styles/PhotoGalleryPage.module.css";
 
 export const PhotoCard = ({ photo, name, description, like }) => {
   const [showDescription, setShowDescription] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
   const onClick = () => {
     setShowDescription(!showDescription);
+    setModalOpen(true);
   };
   const [isLike, setLike] = useState(false);
   const [numLike, setNumLike] = useState(like);
@@ -39,16 +44,17 @@ export const PhotoCard = ({ photo, name, description, like }) => {
   };
 
   return (
-    <div className={style.cards}>
-      <Card className="col">
-        <CardImg
-          src={photo}
-          alt={name}
-          width="100%"
-          height="100%"
-          onClick={onClick}
-        />
-        <CardBody>
+    <>
+      <Modal centered isOpen={modalOpen}>
+        <CardImg src={photo} alt={name} width="90%" height="90%" />
+        <CardImgOverlay className={style.quit}>
+          <i
+            class="fa fa-times"
+            aria-hidden="true"
+            onClick={() => setModalOpen(false)}
+          ></i>
+        </CardImgOverlay>
+        <ModalBody>
           {isLike ? (
             <i
               className="fa fa-heart text-danger"
@@ -66,64 +72,94 @@ export const PhotoCard = ({ photo, name, description, like }) => {
             {" "}
             {numLike} {numLike > 1 ? "likes" : "like"}{" "}
           </span>
-          <br />
-          <div>
-            {commentList.map((comment) => (
-              <>
-                <div>
-                  <b>{comment.username}</b> {comment.comment} {"("}
-                  {comment.rate}
-                  {")"}
-                </div>
-              </>
-            ))}
-          </div>
-          <Label className={style.label}>Username :</Label>
-          <Input
-            type="text"
-            className={style.input}
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            invalid={username == ""}
-            valid={username != ""}
+        </ModalBody>
+      </Modal>
+      <div className={style.cards}>
+        <Card className="col">
+          <CardImg
+            src={photo}
+            alt={name}
+            width="100%"
+            height="100%"
+            onClick={onClick}
           />
-          <Label className={style.label}>Comment :</Label>
-          <Input
-            type="text"
-            className={style.input}
-            value={comment}
-            invalid={comment == ""}
-            valid={comment != ""}
-            onChange={(e) => setComment(e.target.value)}
-          />
-          <Label className={style.label}>Rating :</Label>
-          <Input
-            className={style.input}
-            type="select"
-            value={rate}
-            onChange={(e) => setRate(e.target.value)}
-            invalid={rate == 0}
-            valid={rate > 0}
-          >
-            <option>0</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </Input>
-          <Button className={style.commentButton} onClick={addComment}>
-            add your comment
-          </Button>
-        </CardBody>
-        {showDescription ? (
           <CardBody>
-            <CardSubtitle>{description}</CardSubtitle>
+            {isLike ? (
+              <i
+                className="fa fa-heart text-danger"
+                aria-hidden="true"
+                onClick={dislike}
+              ></i>
+            ) : (
+              <i
+                className="fa fa-heart-o"
+                aria-hidden="true"
+                onClick={addLike}
+              ></i>
+            )}
+            <span>
+              {" "}
+              {numLike} {numLike > 1 ? "likes" : "like"}{" "}
+            </span>
+            <br />
+            <div>
+              {commentList.map((comment) => (
+                <>
+                  <div>
+                    <b>{comment.username}</b> {comment.comment} {"("}
+                    {comment.rate}
+                    {")"}
+                  </div>
+                </>
+              ))}
+            </div>
+            <Label className={style.label}>Username :</Label>
+            <Input
+              type="text"
+              className={style.input}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              invalid={username == ""}
+              valid={username != ""}
+            />
+            <Label className={style.label}>Comment :</Label>
+            <Input
+              type="text"
+              className={style.input}
+              value={comment}
+              invalid={comment == ""}
+              valid={comment != ""}
+              onChange={(e) => setComment(e.target.value)}
+            />
+            <Label className={style.label}>Rating :</Label>
+            <Input
+              className={style.input}
+              type="select"
+              value={rate}
+              onChange={(e) => setRate(e.target.value)}
+              invalid={rate == 0}
+              valid={rate > 0}
+            >
+              <option>0</option>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Input>
+            <Button className={style.commentButton} onClick={addComment}>
+              add your comment
+            </Button>
           </CardBody>
-        ) : (
-          <div></div>
-        )}
-      </Card>
-    </div>
+          {showDescription ? (
+            <CardBody>
+              <CardSubtitle>{description}</CardSubtitle>
+            </CardBody>
+          ) : (
+            <div></div>
+          )}
+        </Card>
+      </div>
+    </>
   );
 };
